d5339508b51ebafa1b7f0e50892e5d09d42934dc
Paweł Kalisz <pawel.kalisz@inspirelabs.pl>
2019-12-01 11:19:17 +0100

    testing commit aND PUSH
    Signed-off-by: Paweł Kalisz <pawel.kalisz@inspirelabs.pl>

--- page.php
+++ page.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-        <?php //test git
+        <?php //test gitUUU
 		// Start the loop.
 		while ( have_posts() ) :
 			the_post();
