6bd1251e2002f53fdaf3c211f409f0ad6e7fdeaa
Paweł Kalisz <pawel.kalisz@inspirelabs.pl>
2019-12-05 11:43:50 +0100

    acf form create and save val
    Signed-off-by: Paweł Kalisz <pawel.kalisz@inspirelabs.pl>

--- single.php
+++ single.php
@@ -1,59 +1,44 @@
-<?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<?php acf_form_head(); ?>
+<?php get_header(); ?>
+    <!-- https://www.advancedcustomfields.com/resources/acf_form/
 
-get_header(); ?>
+     -->
+    <div id="primary" class="content-area">
+        <div id="content" class="site-content" role="main">
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) :
-			the_post();
+            <?php /* The loop */ ?>
+            <?php while (have_posts()) : the_post(); ?>
 
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
+                <?php /*
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
+                    for mularz tworzy nowe posty z typu posta event
 
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation(
-					array(
-						'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-					)
-				);
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation(
-					array(
-						'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-							'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-							'<span class="post-title">%title</span>',
-						'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-							'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-							'<span class="post-title">%title</span>',
-					)
-				);
-			}
+    `               acf_form(array(
+                    'post_id'		=> 'new_post',
+                    'new_post'		=> array(
+                        'post_type'		=> 'travel',
+                        'post_status'		=> 'publish'
+                    ),
+                    'submit_value'		=> 'Create a new travel'
+                ));
+ */ ?>
 
-			// End of the loop.
-		endwhile;
-		?>
+                <?php //dodawanie wlasnych pol i zachowanie ich wartosci
+                acf_form(array(
 
-	</main><!-- .site-main -->
+                    'field_groups' => array('forms'),
+                    'fields' => array('textform1', 'textform2'),
+                    'submit_value' => 'save',
+                    'updated_message' => __("Post updated", 'acf'),
+                    'html_submit_spinner' => '<span class="acf-spinner"></span>',
+                    'kses' => true
+                )); ?>
 
-	<?php get_sidebar( 'content-bottom' ); ?>
 
-</div><!-- .content-area -->
+            <?php endwhile; ?>
+
+        </div><!-- #content -->
+    </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
