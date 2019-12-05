1a668223cd610dac9d8a02fdaf1b448a3814d29f
Paweł Kalisz <pawel.kalisz@inspirelabs.pl>
2019-12-05 12:00:36 +0100

    install acf plugin inside theme
    Signed-off-by: Paweł Kalisz <pawel.kalisz@inspirelabs.pl>

--- functions.php
+++ functions.php
@@ -585,3 +585,27 @@ function my_acf_google_map_api($api)
 }
 
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+
+
+// add ACF with theme
+// https://www.advancedcustomfields.com/resources/including-acf-within-a-plugin-or-theme/
+
+// Define path and URL to the ACF plugin.
+define('MY_ACF_PATH', get_stylesheet_directory() . '/inc/acf/');
+define('MY_ACF_URL', get_stylesheet_directory_uri() . '/inc/acf/');
+
+// Include the ACF plugin.
+include_once(MY_ACF_PATH . 'acf.php');
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url($url)
+{
+    return MY_ACF_URL;
+}
+// (Optional) Hide the ACF admin menu item.
+/*add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+function my_acf_settings_show_admin( $show_admin ) {
+    return false;
+}
+*/
